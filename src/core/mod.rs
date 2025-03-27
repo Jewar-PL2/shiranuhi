96bc9cc305d6362610fa36529e8f02e894476a0f
Jewar-PL2 <a.batorski.jewar@gmail.com>
2025-03-27 17:29:25 +0100

    Added an utility Range struct

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,16 @@
 pub mod cpu;
 pub mod bus;
 pub mod bios;
+
+pub struct Range(u32, u32);
+impl Range {
+    pub fn contains(self, address: u32) -> Option<u32> {
+        let Range(start, size) = self;
+
+        if address >= start && address < start + size {
+            Some(address - start)
+        } else {
+            None
+        }
+    }
+}
